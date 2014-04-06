69aa1bfe8f3d41db371ae50fa771d4070dc33933
Liyuan.Hao <lhao@millennialmedia.com>
2014-04-06 13:26:51 -0400

    initial extended classes

--- rootcause/models.py
+++ rootcause/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from mezzanine.forms.models import Form, Field, FormEntry
+from mezzanine.utils.models import get_user_model_name
 
-# Create your models here.
-from django.forms import ModelForm
+class AppForm(Form):
+    pass
 
+class AppField(Field):
+    pass
+
+class AppFormEntry(FormEntry):
+    user = models.ForeignKey(get_user_model_name(), blank=True, null=True)
 
 class Track(models.Model):
     name = models.CharField(max_length=120, unique=True)
     expired_at = models.DateTimeField(null = True, blank=True)
-
 
     def __str__(self):
         return self.name
