78b7abf819ca0df6ea298e2af4bc3d975a2702ef
Stephen Abrams <abrams.stephen@gmail.com>
2014-04-26 23:06:28 -0400

    steves fears

--- snapp/signals.py
+++ snapp/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from forms_builder.forms.signals import form_valid
 
 
+# todo: what if this fails?
 @receiver(form_valid)
 def set_username(sender=None, form=None, entry=None, **kwargs):
     request = sender
@@ -10,3 +11,4 @@ def set_username(sender=None, form=None, entry=None, **kwargs):
         field_entry, _ = entry.fields.get_or_create(field_id=field.id)
         field_entry.value = request.user.id
         field_entry.save()
+
