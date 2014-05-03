0d79eab41124e392a414a3f29c5f23d72251bed0
Stephen Abrams <abrams.stephen@gmail.com>
2014-05-03 12:39:22 -0400

    add links to forms on index page, require auth (still sandbox work here)

--- snapp/views.py
+++ snapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from forms_builder.forms.models import Form
 
 # Create your views here.
 
-def index(request):
-    x = "YOU!"
+from django.contrib.auth.decorators import login_required
 
-    context = {'x': x}
+@login_required
+def index(request):
+    form_list = Form.objects.all();
+
+    username = request.user.username
+    context = {'username': username, 'forms': form_list}
+
     return render(request, 'snapp/index.html', context)
