ab44f5d61a77b74f31ce79d1111b04a843b031fe
Liyuan.Hao <lhao@millennialmedia.com>
2014-05-03 19:50:16 -0400

    submitted form; navigation bar

--- snapp/views.py
+++ snapp/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
 from forms_builder.forms.models import Form
+from forms_builder.forms.models import Field
 from forms_builder.forms.models import FormEntry
+from forms_builder.forms.models import FieldEntry
+
 
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def index(request):
     forms = Form.objects.all()
-
-    username = request.user.username
-    context = {'username': username, 'forms': forms}
+    form_entry = FormEntry.objects.filter(user=request.user)[0]
+    context = {'user': request.user, 'forms': forms, 'form_entry': form_entry}
 
     return render(request, 'snapp/index.html', context)
+
+
+@login_required
+def submitted_form_entry(request, form_entry_id):
+    form_entry = FormEntry.objects.get(pk=form_entry_id)
+    rows = []
+    for field_entry in form_entry.fields.all():
+        row={'label': form_entry.label_for_field(field_entry), 'value': field_entry.value}
+        rows.append(row)
+    context = {'form_entry': form_entry, 'rows': rows}
+
+    return render(request, 'snapp/submitted_form.html', context)
 
 @login_required
 def evaluation_dashboard(request):
     form_entries = FormEntry.objects.all()
 
     username = request.user.username
-    context = {'username': username, 'form_entries': form_entries}
+    context = {'user': request.user, 'form_entries': form_entries}
 
     return render(request, 'snapp/evaluation_dashboard.html', context)
+
 
 @login_required
 def evaluation_form(request, form_entry_id):
