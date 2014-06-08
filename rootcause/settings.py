b8cb9d9fa6b9ad349cdc7c3d61fa7486076f4e4f
Pradeep Raman <pradeep.raman@jumptap.com>
2014-06-08 17:40:29 -0400

    Revert "fixes #48"
    This reverts commit cb65142daa0c183c6d6dfd62a001d37ced519447.

--- rootcause/settings.py
+++ rootcause/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -18,11 +21,11 @@ import os
 SECRET_KEY = 'am1z6gj0qk0wy_q#0@5m7+_dutsd%4kcxv+)xvd3zwy=la+0uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 # and https://devcenter.heroku.com/articles/getting-started-with-django#start-a-django-app-inside-a-virtualenv
 # Static asset configuration
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
