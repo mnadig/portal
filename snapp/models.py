22062649850f165d9b8914dd6a4d5fccf35f4f9b
Pradeep Raman <pradeep.raman@jumptap.com>
2014-07-12 16:25:47 -0400

    fixes #56

--- snapp/models.py
+++ snapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_enumfield import enum
 
 class Track(models.Model):
     name = models.CharField(max_length=50)
@@ -8,3 +10,23 @@ class Track(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class ApplicationStatus(enum.Enum):
+    SUBMITTED_PHASE1 = 0
+    APPROVED_PHASE1 = 1
+    REJECTED_PHASE1 = 2
+
+    SUBMITTED_PHASE2 = 3
+    APPROVED_PHASE2 = 4
+    REJECTED_PHASE2 = 5
+
+    _transitions = {
+        SUBMITTED_PHASE1: (APPROVED_PHASE1,REJECTED_PHASE1,),
+        APPROVED_PHASE1: (SUBMITTED_PHASE2,),
+        SUBMITTED_PHASE2: (APPROVED_PHASE2,REJECTED_PHASE2,),
+    }
+
+class Application(models.Model):
+   user = models.ForeignKey(User)
+   track = models.ForeignKey(Track)
+   status =  enum.EnumField(ApplicationStatus)
