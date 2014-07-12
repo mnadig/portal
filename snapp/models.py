dc41c1fa3b1b43ff834a8940178ecaa1f75b17ab
Pradeep Raman <pradeep.raman@jumptap.com>
2014-07-12 15:21:35 -0400

    fixes #55

--- snapp/models.py
+++ snapp/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Track(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    form = models.ForeignKey("forms.Form")
+    form = models.ForeignKey("forms.Form", related_name='Phase 1 Form', verbose_name='Phase 1 Form')
+    form_phase2 = models.ForeignKey("forms.Form", related_name='Phase 2 Form', verbose_name='Phase 2 Form')
 
     def __unicode__(self):
         return self.name
